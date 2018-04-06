1fb80cf744870c953f9d971476838b914e2607a6
Žiga Krašovec <zkrasovec@gmail.com>
2018-04-06 13:06:45 +0200

    Finished Chapter02
    Added methods and bindings for reactivity of prototype application

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -3,6 +3,36 @@ const app = new Vue({
 	data: {
 		title: sample.title,
 		address: sample.address,
-		about: sample.about
+		about: sample.about,
+		headerImageStyle: {
+			'background-image': 'url(sample/header.jpg)'
+		},
+		amenities: sample.amenities,
+		prices: sample.prices,
+		contracted: true,
+		modalOpen: false
+	},
+	watch: {
+		modalOpen() {
+			const className = 'modal-open';
+			if (this.modalOpen) {
+				document.body.classList.add(className);
+			} else {
+				document.body.classList.remove(className);
+			}
+		}
+	},
+	methods: {
+		escapeKeyListener(e) {
+			if (e.keyCode === 27 && this.modalOpen) {
+				this.modalOpen = false;
+			}
+		}
+	},
+	created() {
+		document.addEventListener('keyup', this.escapeKeyListener);
+	},
+	destroyed() {
+		document.removeEventListener('keyup', this.escapeKeyListener);
 	}
 });
